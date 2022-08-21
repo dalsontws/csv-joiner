30f6646bd46ce828c3bfdb920188ea50bd7c0d91
dalsontws <dalsontws@gmail.com>
2022-08-22 00:05:23 +0800

    feat: add column to join input

--- main.py
+++ main.py
@@ -4,11 +4,9 @@ from flask import (
     request,
     redirect,
     url_for,
-    send_file,
     Response,
 )
 import pandas as pd
-
 
 app = Flask(__name__)
 
@@ -22,20 +20,26 @@ def index():
 def upload_file():
     uploaded_first_file = request.files["first_csv"]
     uploaded_second_file = request.files["second_csv"]
-    if uploaded_first_file.filename != "" and uploaded_second_file.filename != "":
+    column_to_join = request.form["column_to_join"]
+    if (
+        uploaded_first_file.filename != ""
+        and uploaded_second_file.filename != ""
+        and column_to_join != ""
+    ):
         # Merge them
-        return join_csv(uploaded_first_file, uploaded_second_file)
+
+        return join_csv(uploaded_first_file, uploaded_second_file, column_to_join)
+
     return redirect(url_for("index"))
 
 
-def join_csv(uploaded_first_file, uploaded_second_file):
+def join_csv(uploaded_first_file, uploaded_second_file, column_to_join):
 
     # reading two csv files
     data1 = pd.read_csv(uploaded_first_file)
     data2 = pd.read_csv(uploaded_second_file)
 
-    # using merge function by setting how='inner'
-    output1 = pd.merge(data1, data2, on="id", how="outer")
+    output1 = pd.merge(data1, data2, on=column_to_join, how="outer")
 
     return Response(
         output1.to_csv(),
